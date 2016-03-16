6d8434bc160dd8e2d8edd53ad231469723ceac65
Sean Oh <seanoh@Seans-MacBook-Pro.local>
2016-03-16 14:03:40 -0700

    Populate listView with fake data via adapter

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -50,6 +56,7 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> mForecastAdapter;
         public PlaceholderFragment() {
         }
 
@@ -57,6 +64,13 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            String[] fakeData = {"today", "tmrw", "w", "th", "f", "s", "s"};
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(fakeData));
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview,
+                    weekForecast);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }

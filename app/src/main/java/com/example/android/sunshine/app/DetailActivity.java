d56b3d69416766b60594ac7c44214c5e20ecd592
Sean Oh <seanoh@Seans-MacBook-Pro-3.local>
2016-05-08 06:47:00 -0700

    Update UI, clean up code

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -19,7 +19,6 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
